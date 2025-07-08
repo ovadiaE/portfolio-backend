e1a1d1b8daf5ef8f5e67f7152535f2969131ef6f
ovadiaE <ovadia.esq@gmail.com>
2025-07-08 14:55:58 +0300

    added pg connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,52 @@
-    // src/app.ts
-    import express, { Request, Response } from "express";
+// src/app.ts
+import express, { Request, Response } from "express";
+import routes from './routes/endpoints'
+import dotenv from 'dotenv';
 
-    import routes from './routes/endpoints'
-   
-    const app = express();
-    const port = process.env.PORT || 3000; 
+const { Client } = require("pg");
+    
+dotenv.config();
+const app = express();
 
-    app.use('/', routes);
+const dbConfig = new Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
 
-    app.listen(port, () => {
-      console.log(`Server running on http://localhost:${port}`);
+const client = new Client(dbConfig);
+    
+const port = process.env.PORT || 3000; 
+
+app.use('/', routes);
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL database");
+
+    // Execute SQL queries here
+
+    client.query("SELECT * FROM job_tracking", (err:any, result:any) => {
+      if (err) {
+        console.error("Error executing query", err);
+      } else {
+        console.log("Query result:", result.rows);
+      }
+
+      // Close the connection when done
+      client
+        .end()
+        .then(() => {
+          console.log("Connection to PostgreSQL closed");
+        })
+        .catch((err:any) => {
+          console.error("Error closing connection", err);
+        });
     });
+  })
+  .catch((err:any) => {
+    console.error("Error connecting to PostgreSQL database", err);
+  })
